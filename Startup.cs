3ecb4b8450753a7129bc1aed10c04c83cceb214c
Shawn Anderton <Shawn Anderton>
2017-02-15 19:14:20 -0700

    second init

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace AngularSignalr
 {
@@ -16,6 +12,12 @@ namespace AngularSignalr
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+        services
+                .AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver = 
+                    new DefaultContractResolver());
+
+            services.AddSignalR(options => options.Hubs.EnableDetailedErrors = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
